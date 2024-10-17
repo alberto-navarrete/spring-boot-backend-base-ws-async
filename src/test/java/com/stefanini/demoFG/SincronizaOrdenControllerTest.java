4fe6578b07b95a35184aa39ab474bfaf14b99586
AlbertoNG <anikore@gmail.com>
2024-10-16 18:30:35 -0600

    Se cambia configuracion del pool por el properties en lugar de la clase

--- src/test/java/com/stefanini/demoFG/SincronizaOrdenControllerTest.java
+++ src/test/java/com/stefanini/demoFG/SincronizaOrdenControllerTest.java
@@ -13,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -45,7 +46,10 @@ public class SincronizaOrdenControllerTest {
     	
         when(coService.llamaServicioSincronizaInformacion("")).thenReturn(valueFuture);
 
-        mockMvc.perform(post("/sincronizaOrden/hola"))
+        mockMvc.perform(post("/sincroniza/orden")
+        		.contentType(MediaType.APPLICATION_JSON)
+                .content("1234")
+                .accept(MediaType.APPLICATION_JSON))
                .andExpect(status().isOk());
     }
 
